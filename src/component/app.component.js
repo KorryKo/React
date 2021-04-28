60edcb99eaee38ab4907151b73eed3e1384f3ed2
Marina <maryaleks@mail.ru>
2021-04-28 22:14:11 +0300

    Homework 1

---
+++ src/component/app.component.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import css from "./app.component.css";
+
+
+const myComponent =()=>{
+    return <div className={css.intro}>Hello World</div>;
+}
+export default myComponent;
