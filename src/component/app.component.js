457018daa1907925b99ba032a26754bb67e3c7e9
Marina <maryaleks@mail.ru>
2021-05-01 12:18:44 +0300

    Homework 1 extrs task

--- src/component/app.component.js
+++ src/component/app.component.js
@@ -1,8 +1,18 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import css from "./app.component.css";
 
-
 const myComponent =()=>{
-    return <div className={css.intro}>Hello World</div>;
+    const [messages, setMessages] = useState([]) ;
+    const addMessage = ()=>{
+        let messageInput = document.getElementById('messageInput').value
+        setMessages(oldMessage =>[...oldMessage, messageInput])
+    }
+    return [
+        <input type="text" id="messageInput"></input>,
+        <button className={css.intro} onClick={addMessage}>
+            Send
+        </button>,
+        <div>{messages.map(message=> <p>{message}</p>)}</div>
+    ];
 }
 export default myComponent;
