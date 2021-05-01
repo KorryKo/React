5f676a6f9d2a7d60dbd2dfe6ceefd795c249960a
Marina <maryaleks@mail.ru>
2021-05-01 20:20:39 +0300

    Homework 1 extrs task add key

--- src/component/app.component.js
+++ src/component/app.component.js
@@ -2,17 +2,22 @@ import React, { useState } from "react";
 import css from "./app.component.css";
 
 const myComponent =()=>{
-    const [messages, setMessages] = useState([]) ;
-    const addMessage = ()=>{
-        let messageInput = document.getElementById('messageInput').value
-        setMessages(oldMessage =>[...oldMessage, messageInput])
+    const [messages, setMessages] = useState([]);
+    const [message, setMessage] = useState();
+    const [messageKey, setMessageKey] = useState(0)
+    const receiveMessage = (receivedMessage) =>{
+       setMessage(receivedMessage.target.value)
     }
-    return [
-        <input type="text" id="messageInput"></input>,
-        <button className={css.intro} onClick={addMessage}>
+    const addMessage = ()=>{
+        setMessageKey(messageKey + 1)
+        setMessages(oldMessage =>[...oldMessage, {id:messageKey,value:message}]);
+    }
+    return (<div>
+            <input type="text" onChange={receiveMessage} id="messageInput"></input>
+            <button className={css.intro} onClick={addMessage}>
             Send
-        </button>,
-        <div>{messages.map(message=> <p>{message}</p>)}</div>
-    ];
+            </button>
+            <div>{messages.map(message=> <p key={message.id}>{message.value}</p>)}</div>
+        </div>)
 }
 export default myComponent;
