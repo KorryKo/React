b1984cd9418a55f3d262aa3cefbef8712e4bb64a
Marina <maryaleks@mail.ru>
2021-05-24 09:38:16 +0300

    homework 7

--- src/component/profile/index.js
+++ src/component/profile/index.js
@@ -1,19 +1,36 @@
 import React from "react"
+import {useEffect, useState} from "react"
 import css from "./styles.css";
 import Paper from '@material-ui/core/Paper';
-import {useSelector} from 'react-redux'
+import {randomProfile} from "../../api";
 
 const Profile = () => {
-    const name = useSelector(store => store.profile.name);
+    const [profileData, setProfileData] = useState({
+        picture: {
+            medium: "",
+            large: ""
+        },
+        name: {
+            first: "",
+            last: ""
+        }
+    });
 
+    useEffect(() => {
+        randomProfile().then(newProfileData => {
+            setProfileData(newProfileData.data.results[0]);
+        })
+    }, []);
     return (
         <Paper elevation={1} className={css.profile}>
             <div className={css.content}>
                 <h1>Profile</h1>
-                <p>Name:{name}</p>
+                <img className={css.img} src={profileData.picture.large} alt=""/>
+                <p>Name: {profileData.name.first}</p>
+                <p>Last Name: {profileData.name.last}</p>
             </div>
         </Paper>
     )
-}
+};
 
 export default Profile
